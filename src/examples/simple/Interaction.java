5d8bde7435a042d50bb99207674f1af28c90bc7a
Dave Briccetti <daveb@davebsoft.com>
2020-05-27 15:17:19 -0700

    Create more examples

---
+++ src/examples/simple/Interaction.java
@@ -0,0 +1,12 @@
+package examples.simple;
+
+import java.util.Scanner;
+
+public class Interaction {
+  public static void main(String[] args) {
+    var scanner = new Scanner(System.in);
+    System.out.println("What is your name?");
+    var name = scanner.nextLine();
+    System.out.println("Hello, " + name);
+  }
+}
