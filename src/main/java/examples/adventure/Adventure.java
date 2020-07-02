db22a6390065e425767dc6133d3d286925857a0f
Dave Briccetti <daveb@davebsoft.com>
2020-07-01 18:33:14 -0700

    Add/update examples

--- src/main/java/examples/adventure/Adventure.java
+++ src/main/java/examples/adventure/Adventure.java
@@ -9,18 +9,41 @@ import static java.lang.System.out;
 
 /** This very simple text adventure game uses Lists and Maps to store information about the world. */
 public class Adventure {
+
+  static class Place {
+    private final String name;
+    private final String description;
+
+    Place(String name, String description) {
+      this.name = name;
+      this.description = description;
+    }
+
+    @Override public String toString() {
+      return name + ": " + description;
+    }
+  }
+
   public static void main(String[] args) {
-    var location = "Farm";
+    new Adventure().play();
+  }
+
+  public void play() {
+    var farm = new Place("Farm", "an old farm with animals");
+    var town = new Place("Town", "a small town in the middle of nowhere");
+    var airport = new Place("Airport", "a one-runway airport, mostly used by crop dusters");
+    var location = farm;
     var transitions = Map.of(
-        "Farm",     List.of("Town"),
-        "Town",     List.of("Farm", "Airport"),
-        "Airport",  List.of("Town")
+        farm,     List.of(town),
+        town,     List.of(farm, airport),
+        airport,  List.of(town)
     );
     var scanner = new Scanner(System.in);
-    while (! location.isEmpty()) {
-      out.println("You are at the " + location);
 
-      // Some event happens with a certain probability
+    while (true) {
+      out.println("You are at " + location);
+
+      // Some event happens with a given probability
       if (new Random().nextFloat() < 0.5) {
         out.println("You find a pot of gold");
       }
@@ -29,8 +52,9 @@ public class Adventure {
       out.println("You can go to " + places);
       out.println("Where to? ");
       var response = scanner.nextLine();
-      if (places.contains(response)) {
-        location = response;
+      var maybePlace = places.stream().filter(place -> place.name.equalsIgnoreCase(response)).findFirst();
+      if (maybePlace.isPresent()) {
+        location = maybePlace.get();
       } else {
         out.println("You can't go there.");
       }
