76632fe82d9b8b757ced823140fab086d6ca0260
Dave Briccetti <daveb@davebsoft.com>
2020-10-14 18:50:29 -0700

    Add List example

---
+++ src/main/java/examples/simple/ListPractice.java
@@ -0,0 +1,33 @@
+package examples.simple;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListPractice {
+  public static void main(String[] args) {
+    var app = new ListPractice();
+    app.practice();
+  }
+
+  private void practice() {
+    // Create an empty list
+    // Learn about List and ArrayList
+    List<Integer> numbers = new ArrayList<>();
+
+    // Add to a list
+    numbers.add(5);
+    numbers.add(6);
+    numbers.add(7);
+    numbers.addAll(List.of(4, 8, 9));
+
+    // Loop over a list’s items
+    for (int number : numbers) {
+      System.out.println(number);
+    }
+    // Integer vs. int: two ways to hold integers, with the first used for Lists
+
+    // Access a specific element of the list
+    System.out.println(numbers.get(1));
+
+  }
+}
