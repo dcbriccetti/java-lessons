90d45d06f2bcd251bb5cf07d4147c6d8ad47a033
Dave Briccetti <daveb@davebsoft.com>
2021-01-06 19:52:12 -0800

    Add numbers to the quiz items

--- src/main/java/examples/simple/Quiz.java
+++ src/main/java/examples/simple/Quiz.java
@@ -4,40 +4,40 @@ import static java.lang.System.out;
 
 
 public class Quiz {
-  // Create a field called temperature and set it to 22.5
+  // 1. Create a field called temperature and set it to 22.5
 
   public static void main(String[] args) {
     out.println("Hello");
 
-    // Create an int variable called speed and set it to 105
+    // 2. Create an int variable called speed and set it to 105
 
 
-    // Print the numbers from -5 to 5 using a loop or a Stream
+    // 3. Print the numbers from -5 to 5 using a loop or a Stream
 
 
-    // Create a function that returns the sum of two numbers. Call it and print the result.
+    // 4. Create a function that returns the sum of two numbers. Call it and print the result.
 
 
-    // Compute the result of throwing two simulated 6-sided dice
+    // 5. Compute the result of throwing two simulated 6-sided dice
 
 
-    // Write a line of code to set the following variable to its value plus 1
+    // 6. Write a line of code to set the following variable to its value plus 1
     int counter = 0;
 
-    // Write a line of code to set the following variable to its value plus 2
+    // 7. Write a line of code to set the following variable to its value plus 2
     int counter2 = 0;
 
 
-    // Write code to print “yes” if counter2 equals 2 or “no” if not
+    // 8. Write code to print “yes” if counter2 equals 2 or “no” if not
 
 
-    // Use a regular expression to extract the name and height from the following: "Name: Sue, Height: 150"
+    // 9. Use a regular expression to extract the name and height from the following: "Name: Sue, Height: 150"
 
 
-    // Display the extracted name and height using printf
+    // 10. Display the extracted name and height using printf
 
 
-    // Create a list containing the current temperatures (in Celsius) of several Bay Area cities
+    // 11. Create a list containing the current temperatures (in Celsius) of several Bay Area cities
 
   }
 }
